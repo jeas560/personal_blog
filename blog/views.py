0fc92ecf2691cdfb8447303d6e3c9b63518915ad
jeas560 <jeas560@gmail.com>
2024-04-17 16:47:05 -0300

    Second step of the blog

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from blog.models import Post
 
-# Create your views here.
+def blog_index(request):
+    posts = Post.objects.all()
+    context = {
+        "posts": posts,
+    }
+    return render(request, "blog/index.html", context)
+
+def blog_tag(request, tag):
+    posts = Post.objects.filter(
+        tags__name__contains=tag
+    )
+    context = {
+        "tag": tag,
+        "posts": posts,
+    }
+    return render(request, "blog/tag.html", context)
+
+def blog_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    context = {
+        "post": post,
+    }
+
+    return render(request, "blog/detail.html", context)
